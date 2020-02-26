f7dd95900edff11e0dd4909690fa7f471dc2f00c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-26 14:12:58 +0200

    init

---
+++ test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Mykhailo Bondarenko on 26.02.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
